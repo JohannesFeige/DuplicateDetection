e5587e44e51109a4d63ab1707951b0a0147c3bbb
DuplicateDetection <johannesfeige@gmail.com>
2019-09-01 22:31:05 +0200

    first draft for implementation in console

--- DuplicateDetection.Console/Program.cs
+++ DuplicateDetection.Console/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace DuplicateDetection.Console
+﻿using DuplicateDetection.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DuplicateDetection.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            serviceCollection
+                .AddTransient<IDuplicateDetectionService, DuplicateDetectionService>()
+                .AddTransient<IFileCrawler, FileCrawler>()
+                .AddTransient<IFileHashService>(services => new CashingFileHashService(new FileHashService()));
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var duplicateDetectionService = serviceProvider.GetRequiredService<IDuplicateDetectionService>();
+
+            var duplicateFiles = duplicateDetectionService.CollectCandidates(args[0]);
         }
     }
 }
